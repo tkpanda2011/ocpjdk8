9f36d4caa049945a934356fb8056148c82aa3edc
Tripati Kumar <tripati.panda@gspann.com>
2017-11-14 14:08:14 +0530

    From Office Test

---
+++ src/advanced/java/classs/design/EnumDeclareTest.java
@@ -0,0 +1,18 @@
+package advanced.java.classs.design;
+
+
+public enum Coffee  //1 
+{ ESPRESSO("Very Strong"), MOCHA, LATTE; //2    
+	public String strength; //3    
+	Coffee(String strength) //4   
+	{     this.strength = strength; //5    
+	} 
+	Coffee() {
+		
+	}
+	} 
+	
+
+public class EnumDeclareTest {
+
+}
